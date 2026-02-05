a20371c7e1bbb0e511d2898338b1110e9d7df822
ui-gl <usama.iqbal@greelogix.com>
2026-02-05 11:26:35 +0500

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'LipaBnb') }}</title>
+        <meta name="google-site-verification" content="YSRuX7fcHi58eURq9yMhU3wwVG-5PYkVfOzAYN8hjU4" />
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
