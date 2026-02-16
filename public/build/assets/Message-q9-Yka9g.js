d526cc3278fe08d63e97d89ea20661307e4f531f
ui-gl <usama.iqbal@greelogix.com>
2026-02-16 22:38:33 +0500

    build

--- public/build/assets/Message-q9-Yka9g.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-Dc9wWUMA.js";import{c as s}from"./ArrowDropDown-C0r-Q4UA.js";const t=s(o.jsx("path",{d:"M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m-2 12H6v-2h12zm0-3H6V9h12zm0-3H6V6h12z"}));export{t as M};
---
+++ public/build/assets/Remove-CRjYcuIg.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DPMShFNb.js";import{c as t}from"./ArrowDropDown-Bha2pyt8.js";const e=t(o.jsx("path",{d:"M19 13H5v-2h14z"}));export{e as R};
