ed905601c2481d8e4172bc45c38741ada9d4fbfd
ui-gl <usama.iqbal@greelogix.com>
2026-02-20 11:49:38 +0500

    build

--- public/build/assets/Remove-BTb4ZHFG.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BgE24J6y.js";import{c as t}from"./ArrowDropDown-Bce16KCh.js";const e=t(o.jsx("path",{d:"M19 13H5v-2h14z"}));export{e as R};
---
+++ public/build/assets/Remove-Ck2pclTE.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-UOOPPRb9.js";import{c as t}from"./ArrowDropDown-DdBapB5l.js";const e=t(o.jsx("path",{d:"M19 13H5v-2h14z"}));export{e as R};
