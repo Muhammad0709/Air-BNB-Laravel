929a329724bfdd1fc5a1e2ed047e588d4720b253
ui-gl <usama.iqbal@greelogix.com>
2026-03-05 01:47:37 +0500

    build

---
+++ public/build/assets/MoreVert-D0k6iD3K.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-Cjxos2U9.js";import{c as s}from"./ArrowDropDown-BRsB93VB.js";const c=s(o.jsx("path",{d:"M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2m0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2"}));export{c as M};
