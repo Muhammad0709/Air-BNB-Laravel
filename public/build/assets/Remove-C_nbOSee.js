a32e981f5030d1e60a13d8a6df311f86aa9dd1d7
ui-gl <usama.iqbal@greelogix.com>
2026-03-13 00:05:29 +0500

    build

---
+++ public/build/assets/Remove-C_nbOSee.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-C63OQoDN.js";import{c as s}from"./ArrowDropDown-CSTfdONU.js";const a=s(o.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"})),c=s(o.jsx("path",{d:"M19 13H5v-2h14z"}));export{a as A,c as R};
