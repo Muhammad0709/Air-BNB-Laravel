5af3ea45975c8348ea208e2c997836a4dff61991
ui-gl <usama.iqbal@greelogix.com>
2026-03-30 19:05:54 +0500

    build

---
+++ public/build/assets/InputError-DiPMI3YY.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-voR9BNZs.js";import{T as n}from"./use-language-CvsesQUG.js";function i({message:r}){return r?o.jsx(n,{component:"p",sx:{marginTop:"8px !important",fontSize:"0.875rem",color:"#d32f2f"},children:r}):null}export{i as I};
