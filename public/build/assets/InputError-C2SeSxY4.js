67af03cb6d33733998ce37f4c3dba441fff54c63
ui-gl <usama.iqbal@greelogix.com>
2026-04-08 01:17:14 +0500

    build

--- public/build/assets/InputError-C2SeSxY4.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-C-o0hUnk.js";import{T as n}from"./use-language-ajNJLZBu.js";function i({message:r}){return r?o.jsx(n,{component:"p",sx:{marginTop:"8px !important",fontSize:"0.875rem",color:"#d32f2f"},children:r}):null}export{i as I};
